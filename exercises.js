b7bee6def9acb7b94a7c54268d78dbb7e22ce37b
EC2 Default User <ec2-user@ip-172-31-36-255.ec2.internal>
2019-12-02 05:49:32 +0000

    finished problem set

--- exercises.js
+++ exercises.js
@@ -1,22 +1,38 @@
 // Question 1
-const isUpperCase = (str) => !/[a-z]/.test(str);
+const isUpperCase = ((word) => {
+  const wordArr = word.split('');
+  return wordArr.every((letter)=>letter.match(/[A-Z0-9_! ]/));
+});
 
 // Question 2
-const removeVowels = (arr) => arr.map((word) => word.replace(/[aeiou]/gi, ''));
+const removeVowels = (arrStr)=>{
+  return arrStr.map((char)=>{
+    return char.replace(/[aeiou]/gi, '');
+  });
+};
 
 // Question 3
 const wordCap = (sentence) => {
-  const wordArr = sentence.toLowerCase().split(' ');
-  return wordArr.map((word) => word[0].toUpperCase() + word.substring(1)).join(' ');
+  const sentArr = sentence.split(' ');
+  const capArr = [];
+  sentArr.forEach((word) => {
+    if (word.match(/[a-z]/gi)) {
+      capArr.push(word[0].toUpperCase() + word.substring(1, word.length).toLowerCase());
+    }
+    return word;
+  });
+  return capArr.join(' ');
 };
 
 // Question 4
-const swapCase = (sentence) => {
-  const chars = sentence.split('');
-  return chars.map((char) => {
-    if (char.match(/[a-z]/)) return char.toUpperCase();
-    if (char.match(/[A-Z]/)) return char.toLowerCase();
-    return char;
+const swapCase = (string) => {
+  const strArr = string.split('');
+  return strArr.map((letter)=>{
+    if (letter.match(/[a-z]/g)){
+      return letter.toUpperCase();
+    }else{
+      return letter.toLowerCase();
+    }
   }).join('');
 };
 
@@ -33,29 +49,30 @@ const staggeredCase = (sentence) => {
       newStr += letter;
     }
   }
-
   return newStr;
 };
 
 // Question 6
-const wordLengths = (sentence) => {
-  if (sentence === undefined) return [];
-  if (sentence.length === 0) return [];
-  return sentence.split(' ').map((word) => `${word} ${word.length}`);
+const wordLengths = (string) => {
+  if (string === '') return [];
+  if (string === undefined) return [];
+  return string.split(' ').map((word) => `${word} ${word.length}`);
 };
 
 // Question 7
-const searchWord = (term, text) => {
-  const searchRegex = new RegExp(`\\b${term}\\b`, 'ig');
-  const matches = text.match(searchRegex);
-  return matches ? matches.length : 0;
+const searchWord = (word, text)=>{
+  let newRegEx = new RegExp(`\\b${word}\\b`, 'ig');
+  let totalMatches = text.match(newRegEx);
+  if (totalMatches){
+    return totalMatches.length;
+  }
+  return 0;
 };
 
-// Question 8
-const highlightWord = (term, text) => {
-  const searchRegex = new RegExp(`\\b${term}\\b`, 'ig');
-  return text.replace(searchRegex, (match) => `<strong>${match}</strong>`);
-};
+const highlightWord = (word, text) => {
+  let newRegEx = new RegExp(`\\b${word}\\b`, 'ig');
+  return text.replace(newRegEx, (match) => `<strong>${match}</strong>`)
+}
 
 // The code below ensures that this file can talk to our test file.
 module.exports = {
