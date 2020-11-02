c9b17e657d6ae0266d1a785123fb450d8111ab75
Ann Duong <thuyanannduong@gmail.com>
2020-11-02 13:29:01 -0500

    removed answers

--- exercises.js
+++ exercises.js
@@ -1,60 +1,41 @@
 // Question 1
-const isUpperCase = (str) => !/[a-z]/.test(str);
+const isUpperCase = () => {
+
+};
 
 // Question 2
-const removeVowels = (arr) => arr.map((word) => word.replace(/[aeiou]/gi, ''));
+const removeVowels = () => {
+
+};
 
 // Question 3
-const wordCap = (sentence) => {
-  const wordArr = sentence.toLowerCase().split(' ');
-  return wordArr.map((word) => word[0].toUpperCase() + word.substring(1)).join(' ');
+const wordCap = () => {
+
 };
 
 // Question 4
-const swapCase = (sentence) => {
-  const chars = sentence.split('');
-  return chars.map((char) => {
-    if (char.match(/[a-z]/)) return char.toUpperCase();
-    if (char.match(/[A-Z]/)) return char.toLowerCase();
-    return char;
-  }).join('');
+const swapCase = () => {
+
 };
 
 // Question 5
-const staggeredCase = (sentence) => {
-  let newStr = '';
+const staggeredCase = () => {
 
-  let lastLetterUpper = false;
-  for (let letter of sentence) {
-    if (letter.match(/[a-z]/i)) {
-      lastLetterUpper ? newStr += letter.toLowerCase() : newStr += letter.toUpperCase();
-      lastLetterUpper = !lastLetterUpper;
-    } else {
-      newStr += letter;
-    }
-  }
-
-  return newStr;
 };
 
 // Question 6
-const wordLengths = (sentence) => {
-  if (sentence === undefined) return [];
-  if (sentence.length === 0) return [];
-  return sentence.split(' ').map((word) => `${word} ${word.length}`);
+const wordLengths = () => {
+
 };
 
 // Question 7
-const searchWord = (term, text) => {
-  const searchRegex = new RegExp(`\\b${term}\\b`, 'ig');
-  const matches = text.match(searchRegex);
-  return matches ? matches.length : 0;
+const searchWord = () => {
+
 };
 
 // Question 8
-const highlightWord = (term, text) => {
-  const searchRegex = new RegExp(`\\b${term}\\b`, 'ig');
-  return text.replace(searchRegex, (match) => `<strong>${match}</strong>`);
+const highlightWord = () => {
+
 };
 
 // The code below ensures that this file can talk to our test file.
